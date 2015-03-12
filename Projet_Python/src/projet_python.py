0262ac434140228a4b053738382c8f7336ff12a7
Johanne DELAGE <delage.johanne@hotmail.fr>
2015-03-12 15:50:28 +0100

    Frist commit

---
+++ Projet_Python/src/projet_python.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "Djo"
+__date__ = "$12 mars 2015 15:25:43$"
+
+if __name__ == "__main__":
+    print "Hello World"
