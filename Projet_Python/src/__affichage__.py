81e8e39e0b69c255776abd591aa9c03a5eecdae7
Johanne DELAGE <delage.johanne@hotmail.fr>
2015-04-02 14:55:12 +0200

    Merge package affichage + fonction aff_arbre_simple

--- Projet_Python/src/__affichage__.py
+++ Projet_Python/src/__affichage__.py
@@ -8,9 +8,11 @@ def menu_arbre_pref():
     A => Saisir une nouvelle séquence (a,c,t,g)
     B => Saisir un nouveau entier k
     C => Construire et visualiser l'arbre des préfixes
+    D => Liste des préfixes et leurs occurences
     ....
     Q => Quitter
     """)
+
 
 def aff_seq(seq):
     if seq is None:
@@ -18,13 +20,14 @@ def aff_seq(seq):
     else:
         print("¤ Séquence : "+seq)
 
+
 def aff_k(k):
     if k is None:
         print("¤ Entier : PAS D'ENTIER")
     else:
         print("¤ Entier : "+str(k))
         
-
+# Affiche l'arbe des préfixes, '--'= une profondeure
 def aff_arbre (A,i):
     if A['val']!={}:
         print i*'--',A['val']
@@ -40,11 +43,34 @@ def aff_arbre (A,i):
         if A['g']!={}:
             print 'g',
             aff_arbre(A['g'],i+1)
+            
+# Affihe la liste des préfixes sur une séquence avec un entier k
+def aff_pre(liste):
+    j=0
+    liste_pre=[]
+    liste_occ=[]
+    for n in liste:
+            liste_pre.append(liste[j])
+            liste_occ.append(liste_pre.count(liste[j]))
+            
+            print ("Motif: '"+liste_pre[j]+"'\n"+" Occurence : "+str(liste_occ[j])+"\n")
+            j+=1
+  
 
 def aff_arbre_simple (A,i):
+    """
+    Affiche chaque noeud dispose dans l'arbre 
+    avec la profondeur
+    disposition en speudo arbre 
+    exemple pour un arbre contenant tata et tati on a
+    t
+     ->a
+       ->t
+         ->a
+       ->i
+    """
     #TODO ajouter des | pour voir mieux les noeud de même profondeur et de la même racine
     for lettre in A.keys():
-        print " "
         if i!=0:
          print"\n"+" "*i+"|->",
         else :
