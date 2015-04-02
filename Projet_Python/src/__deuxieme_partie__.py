69a06830e38d77e45f10e9527672ab983cf5f0f0
Charles <Charles@G56>
2015-04-03 01:39:26 +0200

    Ajout sous-menu arbre suffixe Modificatiion de la fonction __Construire_Arbre__ pour fonctionner avec l'affichage et le sous-menu B, pour avoir un arbre retourné

--- Projet_Python/src/__deuxieme_partie__.py
+++ Projet_Python/src/__deuxieme_partie__.py
@@ -19,48 +19,43 @@ def __Insert_Noeud_Successeurs__(A,seq):
     """
     #TODO vérifier qu'il a tout les cas possible et non des supperflus
     
-#    if A is None: #pas de noeud
-#        print "¤ Creation de l'abre ..."
-#        if len(seq)>0: # il reste des lettre
-#            A= __Initialisation_Arbre()
-#            __Insert_Noeud_Successeurs__(A, seq)
     if A=={}: # noeud vide => creer etiquette lettre
-        if len(seq)==1 :#seq est de 1
-            A[seq[0]]={} # cree etiquette
-            return A #nd le plus profond
-        elif len(seq)>1 and seq[0]!='$':#il reste des lettres autres que $
-            A[seq[0]]={}           
-            __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
-        elif seq[0]=='$':
+        if seq[0]=='$':
             #TODO traitement de fin de suffixe
             A['$']={}
             return A
+        elif len(seq)==1 :#seq est de 1
+            A[seq[0]]={} # cree etiquette
+            return A #nd le plus profond
+        else:#il reste des lettres autres que $
+            A[seq[0]]={}           
+            return __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
     elif seq[0] in A  :# Il existe un etiquette de la premiere lettre
         if seq[0]=='$':# Fin de sequence
             #TODO traitement de fin de suffixe
             A['$']={}
             return A
-            print "¤ Arbre construit ."
         elif A[seq[0]]=={}:#etiquette existe vide
             #TODO creer etiquette
             if len(seq[1:])>0: # il reste des lettres après
-                __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
+                return __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
             else:
                 return A
         else: #etiquette existe non vide
             if len(seq[1:])>0: # il reste des lettres après
-                __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])
+                return __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])
     else :# il n'existe pas d'etiquette
         if len(seq)==1 :#seq est de 1
             A[seq[0]]={} # cree etiquette
             return A #nd le plus profond
         elif len(seq)>1 and seq[0]!='$':#il reste des lettres autres que $
             A[seq[0]]={}           
-            __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
+            return __Insert_Noeud_Successeurs__(A[seq[0]], seq[1:])#on passe à la lettre suivante
         else :
             #TODO traitement de fin de suffixe
             A['$']={}
             return A
+    
 
 def __Noeud_Profond__(A,seq):
     """
@@ -82,35 +77,35 @@ def __Is_Racine__(A):
         return True
     
 def __Insert_Suffixe__(N0,seq):
-    print N0
-    print seq
-    if 'suff' in N0:
-        Fils =N0
-        N0=N0['suff']
-        if seq[-1] in Fils:#t existe chez le pere
-            print"hello"
-    else:
-        N0[seq[-1]]={"suff":N0}
-        
+    N=N0
+    if 'suff' in N and seq[-1]!='$':
+        Nprim=N['suff'] 
+        Fils=N[seq[-1]]
+        if 'suff' in Fils:
+            print""
+        else:
+            if seq[-1] in Nprim:
+                Filsprim=Nprim[seq[-1]]
+            else:
+                Nprim[seq[-1]]={}
+                Filsprim=Nprim[seq[-1]]
+            Fils['suff']=Filsprim
+            N=Nprim
+    else: # initialisation
+        if len(seq)==1 and seq[0]!='$':
+            N[seq[0]]={'suff':N}
+
+def __Ajout_Dolar_Prefixe(A,seq):
+    for i in range(len(seq)):
+        #print seq[i:]
+        print seq[i],
+        print i
+        if i+1<len(seq) and seq [i] in A and seq[i]!='$':
+             __Ajout_Dolar_Prefixe(A, seq[i+1:])
             
         
-#    #print N0
-#    if 'succ'
-##    print Fils
-#    if seq[-2] in Fils:
-#        print " suffixe existe"
-#    else:
-#        print " suffixe n'existe pas"
-#        __Insert_Noeud_Successeurs__(Fils,seq[-2]+"$")
-#    if 'succ' in Fils:
-#     print Fils
-#     print N0
-#    if __Is_Racine__(N0)!=True:# il existe un lien successeur = ce n'est pas une racine
-#       if len(seq)>1: # on est pas a la derniere lettre
-#            return A
-#       else:
-#        return "poul"
-#    return A
+        
+
 
 def __Construire_Arbre__(seq):
     from __saisie__ import SaisieADN
@@ -126,15 +121,13 @@ def __Construire_Arbre__(seq):
     A=__Initialisation_Arbre()
     #N0 le noeud le plus profond
     N0=A
-    for i in range(len(seq)+1):
-        if i ==0:
-            N0=__Insert_Noeud_Successeurs__(A,seq[i])
-            #print N0
-            #__Insert_Suffixe__(N0,seq[i])
-        else:
-            N0=__Insert_Noeud_Successeurs__(A,seq[0:i])
-            #print N0
-            #__Insert_Suffixe__(N0,seq[0:i])
+    
+    for i in range(1,len(seq)+1):
+        print seq[:i]
+        N0=__Insert_Noeud_Successeurs__(A,seq[:i])
+        __Insert_Suffixe__(N0,seq[:i])
+    __Ajout_Dolar_Prefixe(A,seq)
+    print A   
 #    seq="attcg$"
 #    for i in range(len(seq)+1):
 #        if i ==0:
@@ -208,4 +201,24 @@ def is_motif(A,q,motif_bis):
 #        if q==motif_bis:
 #            print("Ok")  
 #               
+            
+        if q!='' and q==motif_bis:
+            isit+=1
+            
+    return (isit)
+    
+    
+#    if A!={}and k>=0:
+#        if motif!='' and motif==motif_bis:
+#            print "¤ Nombres d'occurences du motif '",motif,"' dans la séquence : ",A['val']
+#        # Récursivité qui construit les motifs
+#        k-=1
+#        if A['a']!={}:
+#            is_motif(A['a'],motif,k,(motif_bis+'a'))
+#        if A['t']!={}:
+#            is_motif(A['t'],motif,k,(motif_bis+'t'))
+#        if A['c']!={}:
+#            is_motif(A['c'],motif,k,(motif_bis+'c'))
+#        if A['g']!={}:
+#            is_motif(A['g'],motif,k,(motif_bis+'g'))      
             
